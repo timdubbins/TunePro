955de037e305ee5e4e5ce70ee45f91ef7755aa25
tidub <57918941+tidub@users.noreply.github.com>
2022-07-18 00:04:39 +0100

    fix typo

--- TunePro/Views/SplashView.swift
+++ TunePro/Views/SplashView.swift
@@ -31,7 +31,7 @@ struct SplashView: View {
             .opacity(animation == .started ? 1 : 0)
 
         } back: {
-            TuneProView(data: data)
+            TunerView(data: data)
         }
         .padding(.top, 20)
         .padding(5)
